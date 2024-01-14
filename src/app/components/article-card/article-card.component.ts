ff62f2494a937df39178949e1afb7fbca550dd80
laska-adief <laskaadief.la@gmail.com>
2024-01-14 18:13:10 +0700

    [Article Oasis] import date pipe in article-card component

--- src/app/components/article-card/article-card.component.ts
+++ src/app/components/article-card/article-card.component.ts
@@ -1,12 +1,13 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-article-card',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './article-card.component.html',
-  styleUrl: './article-card.component.scss'
+  styleUrl: './article-card.component.scss',
 })
 export class ArticleCardComponent {
-
+  date = new Date();
 }
