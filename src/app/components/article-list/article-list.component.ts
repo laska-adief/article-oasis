6692987074c81cafef62b2249d72e79c1cbde0b7
laska-adief <laskaadief.la@gmail.com>
2024-01-16 14:51:32 +0700

    [Article Oasis] import article-card component and dummy variable for article loop data

--- src/app/components/article-list/article-list.component.ts
+++ src/app/components/article-list/article-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ArticleCardComponent } from '../article-card/article-card.component';
 
 @Component({
   selector: 'app-article-list',
   standalone: true,
-  imports: [],
   templateUrl: './article-list.component.html',
   styleUrl: './article-list.component.scss',
+  imports: [ArticleCardComponent],
 })
-export class ArticleListComponent {}
+export class ArticleListComponent {
+  articles: any = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+}
